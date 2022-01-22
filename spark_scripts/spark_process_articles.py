5040b693f490008efb0e907a2e8f35103fd9d63e
MrDomani <seriousmorgen@protonmail.com>
2022-01-22 17:42:19 +0100

    updated spark_process_articles.py

--- spark_scripts/spark_process_articles.py
+++ spark_scripts/spark_process_articles.py
@@ -47,9 +47,9 @@ annotations_added = annotations.withColumn('confident_positive_sentiment', (anno
                                .withColumn('confident_negative_sentiment', (annotations['sentiment'] == 'negative') & (annotations['sentiment_confidence'] > 0.5)) 
 annotations_added.createOrReplaceTempView('articles_with_sentiment')
 
-print('--- articles_tweets ---')
-print(annotations_added.printSchema())
-print(annotations_added.show())
+#print('--- articles_tweets ---')
+#print(annotations_added.printSchema())
+#print(annotations_added.show())
 
 
 join_tweets_query = """ 
@@ -59,49 +59,51 @@ SELECT articles_with_sentiment.id,
     FIRST(articles_with_sentiment.author) AS author,
     FIRST(articles_with_sentiment.topic) AS topic,
     FIRST(articles_with_sentiment.is_opinion) AS is_opinion,
+    FIRST(articles_with_sentiment.country) AS country,
     FIRST(articles_with_sentiment.confident_positive_sentiment) AS confident_positive_sentiment,
     FIRST(articles_with_sentiment.confident_negative_sentiment) AS confident_negative_sentiment,
-    SUM(CASE tweets.id IS NULL THEN 0 ELSE 1) as article_number_of_tweets
+    SUM(CASE WHEN tweets.id IS NULL THEN 0 ELSE 1 END) as article_number_of_tweets
 FROM tweets RIGHT JOIN articles_with_sentiment ON tweets.article_id=articles_with_sentiment.id
 GROUP BY articles_with_sentiment.id
 """
 articles_tweets = spark.sql(join_tweets_query)
 articles_tweets.createOrReplaceTempView('articles_tweets')
 
-print('--- articles_tweets ---')
-print(articles_tweets.printSchema())
-print(articles_tweets.show())
+#print('--- articles_tweets ---')
+#print(articles_tweets.printSchema())
+#print(articles_tweets.show())
 
 #confident_sentiment = spark.sql('SELECT article_title, sentiment, sentiment_confidence, confident_positive_sentiment, confident_negative_sentiment FROM articles_with_sentiment ORDER BY sentiment_confidence DESC')
 #print(confident_sentiment.show(truncate=False))
 
 # Publisher's summary
 publishers_query = """ 
-SELECT twitter_publisher_name,
-    twitter_publisher_id AS row_key,
+SELECT FIRST(publisher_name) AS publisher_name,
+    publishers.twitter_id AS row_key,
     COUNT(*) AS total_published_articles,
-    SUM(CAST(article_is_opinion AS INT)) AS total_published_opinions,
-    ROUND(SUM(CAST(article_is_opinion AS INT)) / COUNT(*), 3) AS published_opinions_fraction,
-    ROUND(SUM(CAST(confident_positive_sentiment AS INT)) / COUNT(*), 3) AS articles_with_positive_sentiment_fraction,
-    ROUND(SUM(CAST(confident_negative_sentiment AS INT)) / COUNT(*), 3) AS articles_with_negative_sentiment_fraction,
-    MAX(publisher_followers_count) as publisher_followers,
-    SUM(article_number_of_tweets) AS tweets_mentioning_articles_sum
-FROM publishers JOIN articles_with_sentiment ON publishers.article_id=articles_with_sentiment.id
-WHERE twitter_publisher_name IS NOT NULL
-GROUP BY twitter_publisher_id, twitter_publisher_name
+    SUM(CAST(articles_tweets.is_opinion AS INT)) AS total_published_opinions,
+    ROUND(SUM(CAST(articles_tweets.is_opinion AS INT)) / COUNT(*), 3) AS published_opinions_fraction,
+    ROUND(SUM(CAST(articles_tweets.confident_positive_sentiment AS INT)) / COUNT(*), 3) AS articles_with_positive_sentiment_fraction,
+    ROUND(SUM(CAST(articles_tweets.confident_negative_sentiment AS INT)) / COUNT(*), 3) AS articles_with_negative_sentiment_fraction,
+    MAX(publishers.followers_count) as publisher_followers,
+    SUM(articles_tweets.article_number_of_tweets) AS tweets_mentioning_articles_sum
+FROM publishers JOIN articles_tweets ON publishers.article_id=articles_tweets.id
+WHERE publishers.publisher_name IS NOT NULL
+GROUP BY publishers.twitter_id, publishers.publisher_name
 ORDER BY total_published_articles DESC"""
-#publishers = spark.sql(publishers_query)
-print('--- publishers ---')
+publishers = spark.sql(publishers_query)
+
+#print('--- publishers ---')
 #print(publishers.printSchema())
 #print(publishers.show())
 
 #column families: name, article_stats, twiter_stats
-#table = 'publishers_test'
-#write_to_file(publishers.select('row_key', 'twitter_publisher_name'), table, 'name')
-#write_to_file(publishers.select('row_key', 'total_published_articles', 'total_published_opinions', 'published_opinions_fraction', 'articles_with_positive_sentiment_fraction', 'articles_with_negative_sentiment_fraction'),
-#              table, 'article_stats')
-#write_to_file(publishers.select('row_key', 'publisher_followers', 'tweets_mentioning_articles_sum'),
-#              table, 'twitter_stats')
+table = 'publishers'
+write_to_file(publishers.select('row_key', 'publisher_name'), table, 'name')
+write_to_file(publishers.select('row_key', 'total_published_articles', 'total_published_opinions', 'published_opinions_fraction', 'articles_with_positive_sentiment_fraction', 'articles_with_negative_sentiment_fraction'),
+              table, 'article_stats')
+write_to_file(publishers.select('row_key', 'publisher_followers', 'tweets_mentioning_articles_sum'),
+              table, 'twitter_stats')
 
 # topics summary
 topics_query = """ 
@@ -117,13 +119,40 @@ ORDER BY total_published_articles DESC
 """ 
 topics = spark.sql(topics_query)
 
-print('--- topics ---')
-print(topics.printSchema())
-print(topics.show())
+#print('--- topics ---')
+#print(topics.printSchema())
+#print(topics.show())
 
 # column families: article_stats, twitter_stats, name
 
-#table = 'topics'
+table = 'topics'
+write_to_file(topics.select('row_key', 'topic'), table, 'name')
+write_to_file(topics.select('row_key', 'total_published_articles', 'articles_with_positive_sentiment_fraction', 'articles_with_negative_sentiment_fraction'),
+              table, 'article_stats')
+write_to_file(topics.select('row_key', 'tweets_mentioning_articles_sum'), table, 'twitter_stats')
+
+
+# locations summary
+locations_query = """ 
+SELECT country,
+    country AS row_key,
+    COUNT(*) AS total_published_articles,
+    ROUND(SUM(CAST(confident_positive_sentiment AS INT)) / COUNT(*), 3) AS articles_with_positive_sentiment_fraction,
+    ROUND(SUM(CAST(confident_negative_sentiment AS INT)) / COUNT(*), 3) AS articles_with_negative_sentiment_fraction,
+    SUM(article_number_of_tweets) AS tweets_mentioning_articles_sum
+FROM articles_tweets
+GROUP BY country
+ORDER BY total_published_articles DESC
+""" 
+#locations = spark.sql(locations_query)
+
+#print('--- locations ---')
+#print(locations.printSchema())
+#print(locations.show())
+
+# column families: article_stats, twitter_stats, name
+
+#table = 'locations'
 #write_to_file(topics.select('row_key', 'topic'), table, 'name')
 #write_to_file(topics.select('row_key', 'total_published_articles', #'articles_with_positive_sentiment_fraction', 'articles_with_negative_sentiment_fraction'),
 #              table, 'article_stats')
